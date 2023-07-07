02bbb96762ee09b87254f8a921d13341c7f0e426
Thulisile Ngomane <ThuleeN@outlook.com>
2023-07-07 04:24:10 +0200

    Fixed pycode style

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,3 @@ from typing import Iterable, Sequence, List, Tuple
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """returns parameters"""
     return [(i, len(i)) for i in lst]
-  
