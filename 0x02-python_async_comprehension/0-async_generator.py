61ecc18843232af550014cf38fe4c359de27d27e
Thulisile <111003983+LyricalScribe@users.noreply.github.com>
2023-07-15 02:09:08 +0200

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,8 +8,6 @@ from typing import List
 
 async def async_generator() -> List[float]:
     """Coroutine will loop 10 times"""
-    results = []
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
-        results.append(random.uniform(0, 10))
-    return results
+        yield random.uniform(0, 10)
