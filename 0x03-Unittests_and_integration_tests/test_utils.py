ec02a8f988d9f65d2cf55636ff8ec8ae0b6370fe
Thulisile Ngomane <ThuleeN@outlook.com>
2023-10-14 17:39:09 +0200

    fixed pycodestyle

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,7 +9,8 @@ from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test class"""
+    """testing function"""
+
 
 @parameterized.expand(
    [
@@ -18,7 +19,17 @@ class TestAccessNestedMap(unittest.TestCase):
       ({"a": {"b": 2}}, ("a", "b"), 2)
    ]
 )
-def test_access_nested_map(self, nested_map, path, expected_output):
-    "test outcome"
+def test_access_nested_map(self, nested_map, path,
+                           expected_result):
+    """test outcome"""
     result = access_nested_map(nested_map, path)
-    self.assertEqual(result, expected_output)
+    self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_result):
+        """test access_nested_map_exception"""
+        self.assertRaises(expected_result)
